1ebbffc6ff31a4da46c2a11bbe5d54797f46d37e
TianrenWang <broodstar_frank@hotmail.com>
2020-07-26 18:09:50 -0400

    post API for conversation

--- routes/message.js
+++ routes/message.js
@@ -13,7 +13,7 @@ router.get('/', passport.authenticate("jwt", {session: false}), (req, res, next)
       response.msg = "There was an error on getting the conversation";
       res.json(response);
     } else {
-      response.msg = "Conversation retrieved successfuly";
+      response.msg = "Conversation retrieved successfully";
       response.conversation = chatRoom;
       res.json(response);
     }
@@ -23,13 +23,29 @@ router.get('/', passport.authenticate("jwt", {session: false}), (req, res, next)
 // get conversation
 router.get('/:name1/:name2', passport.authenticate("jwt", {session: false}), (req, res, next) => {
   let response = {success: true};
-  Conversation.getConversationByName(req.params.name1, req.params.name2, (err, conversation) => {
+  Conversation.getConversationByParty(req.params.name1, req.params.name2, (err, conversation) => {
     if (err) {
       response.success = false;
       response.msg = "There was an error on getting the conversation";
       res.json(response);
     } else {
       response.msg = "Conversation retrieved successfuly";
+      response.conversation = conversation;
+      res.json(response);
+    }
+  });
+});
+
+// post conversation
+router.post('/', passport.authenticate("jwt", {session: false}), (req, res, next) => {
+  let response = {success: true};
+  Conversation.saveConversation(req.body, (err, conversation) => {
+    if (err) {
+      response.success = false;
+      response.msg = "There was an error saving the conversation";
+      res.json(response);
+    } else {
+      response.msg = "Conversation saved successfully";
       response.conversation = conversation;
       res.json(response);
     }
